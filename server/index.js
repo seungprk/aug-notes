1ac839421489fdb48da6c9af1c7c367147dd41d5
seungprk@gmail.com <seungprk@gmail.com>
2018-05-30 10:16:41 -0700

    Set server to serve index html with data in url

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
+app.use((req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 
 app.listen(port, () => console.log('Listening on port 3000'));
 
